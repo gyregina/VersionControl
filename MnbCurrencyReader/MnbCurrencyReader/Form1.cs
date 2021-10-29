7371f6b263ff9136bd499384d5170a413c4eb3ba
Regina Gyürke <gyregina55@gmail.com>
2021-10-29 10:30:03 +0200

    XML feldolgozása

--- MnbCurrencyReader/MnbCurrencyReader/Form1.cs
+++ MnbCurrencyReader/MnbCurrencyReader/Form1.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace MnbCurrencyReader
 {
@@ -22,10 +23,31 @@ namespace MnbCurrencyReader
         {
             InitializeComponent();
             GetExchangeRates();
+            LoadXml(xmlstring);
             dataGridView1.DataSource = Rates;
+
         }
 
-        private void GetExchangeRates()
+        private void LoadXml(string input)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(input);
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                XmlElement child = (XmlElement)item.FirstChild;
+                r.Currency = child.GetAttribute("curr");
+                r.Value = decimal.Parse(child.InnerText);
+                int unit = int.Parse(child.GetAttribute("unit"));
+                if (unit != 0)
+                    r.Value = r.Value / unit;
+                
+                Rates.Add(r);
+            }
+        }
+
+        public string GetExchangeRates()
         {
             MNBArfolyamServiceSoapClient mnBService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
