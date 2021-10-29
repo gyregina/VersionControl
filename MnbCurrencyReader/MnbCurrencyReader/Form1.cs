1eae651886ed485f67f95a5e67c5fb0dc0dd5257
Regina Gyürke <gyregina55@gmail.com>
2021-10-29 10:09:41 +0200

    Webszolgáltatás hozzáadása és hívása

--- MnbCurrencyReader/MnbCurrencyReader/Form1.cs
+++ MnbCurrencyReader/MnbCurrencyReader/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MnbCurrencyReader.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,12 +11,25 @@ using System.Windows.Forms;
 
 namespace MnbCurrencyReader
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
 
+        }
+
+        private void GetExchangeRates()
+        {
+            MNBArfolyamServiceSoapClient mnBService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020 - 01 - 01";
+            request.endDate = "2020-06-30";
+            var response = mnBService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
         }
     }
 }
