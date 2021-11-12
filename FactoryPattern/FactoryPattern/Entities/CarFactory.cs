9c3c86fa5cf744e106ccb2cdb3f862e42150876c
Regina Gyürke <gyregina55@gmail.com>
2021-11-12 21:17:55 +0100

    carfactory

---
+++ FactoryPattern/FactoryPattern/Entities/CarFactory.cs
@@ -0,0 +1,18 @@
+﻿using FactoryPattern.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Entities
+{
+    public class BallFactory: IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Ball();
+        }
+
+    }
+}
