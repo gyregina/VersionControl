6b36678485fa262ad56a9af283c3a43b0fb3950d
Regina Gyürke <gyregina55@gmail.com>
2021-11-26 10:52:33 +0100

    TestValidatePassword

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -27,8 +27,29 @@ namespace UnitTestExample.Test
             //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
-    }
+        //"A jelszó legalább 8 karakter hosszú kell legyen, 
+        //csak az angol ABC betűiből és számokból állhat, 
+        //és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
+        [
+            Test,
+            TestCase("abcdABCD", false),
+            TestCase("ABCDEFGH4321", false),
+            TestCase("abcd5432", false),
+            TestCase("aB1", false),
+            TestCase("Ab3Cd455", true)
 
+        ]
+
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+            //Act
+            var actualResult = accountController.ValidatePassword(password);
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
 
 
 }
