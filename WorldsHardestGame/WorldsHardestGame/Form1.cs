7178f3b9e7116f46897cc1b65673a56355c2d609
Regina Gyürke <gyregina55@gmail.com>
2021-12-10 10:50:18 +0100

    Induló populáció, iterációs lépés felépítése, új generáció, winner

--- WorldsHardestGame/WorldsHardestGame/Form1.cs
+++ WorldsHardestGame/WorldsHardestGame/Form1.cs
@@ -12,9 +12,74 @@ namespace WorldsHardestGame
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga = null;
+        Brain winnerBrain = null;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
+
+            gc.GameOver += Gc_GameOver;
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            
+            gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = generation.ToString() + ".generáció";
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count()>0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var brain = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(brain.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(brain);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(brain.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(brain.Mutate());
+                
+            } 
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            gc.Start(true);
+
         }
     }
 }
